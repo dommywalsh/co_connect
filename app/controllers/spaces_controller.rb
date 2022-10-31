a21ddefbd7044fd801a3ee4657fbe4b4d82eb2be
jonathandreyer <jonathan.dreyer60@gmail.com>
2022-10-31 17:06:32 +0100

    Updated New

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -6,8 +6,19 @@ class SpacesController < ApplicationController
   end
 
   def new
+    @space = Space.new
   end
 
   def create
+    @space = Space.new(space_params)
+    @space.save
+    # No need for app/views/spaces/create.html.erb
+    redirect_to space_path(@space)
+  end
+  
+  private
+  
+  def space_params
+    params.require(:space).permit(:name, :address, :price)
   end
 end
