0f9e1755d9654c7c763d502775198d5dd54d247a
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-01 15:28:53 +0200

    form is working - yay

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,24 @@
 class BookingsController < ApplicationController
-  def new; end
-  def create; end
+  def new
+    @booking = Booking.new
+    @space = Space.find(params[:space_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    @booking.user = current_user
+    @space = Space.find(params[:space_id])
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:price, :start_date, :end_date, :user_id, :space_id)
+  end
 end
