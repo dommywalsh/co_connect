7729a5807da160a4cdfbcedfb1f49e01c766af17
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-01 12:04:05 +0200

    CRUD actions done

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -5,7 +5,6 @@ class SpacesController < ApplicationController
   end
 
   def show
-    @space = Space.find(params[:id])
   end
 
   def new
@@ -14,16 +13,14 @@ class SpacesController < ApplicationController
 
   def create
     @space = Space.new(space_params)
-    if @space.save
-      redirect_to space_path(@space)
-    else
-      render :new, status: :unprocessable_entity
-    end
+    @space.save
+    # No need for app/views/spaces/create.html.erb
+    redirect_to space_path(@space)
   end
 
   private
 
   def space_params
-    params.require(:space).permit(:name, :address, :price, :description)
+    params.require(:space).permit(:name, :address, :price)
   end
 end
