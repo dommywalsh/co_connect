8cd5ea9e79a6f775fb70d508d2ce4a78485d0456
Ryan Brouwer <mennofromheavan@gmail.com>
2022-11-02 18:48:33 +0200

    ARCH: fixed routes and views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboards/show'
   devise_for :users
   root to: "pages#home"
 
-  resources :spaces, except: [:edit, :update] do
+  resources :spaces do
     resources :bookings, only: [:new, :create, :show]
     # resources :reviews, only: [:new, :create]
   end
