c268dbc8e72381421c9ae3ccc9193496976be35e
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-02 20:58:18 +0200

    dashboard is moving

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,15 @@ class PagesController < ApplicationController
   def dashboard
     @user = current_user
     # @booking = @user.bookings
+
+    # spaces variable
+
     @spaces = @user.spaces
+    # @pending_bookings = @spaces.bookings.select { |booking| booking.status == "pending" }
+    # @confirmed_bookings = @spaces.bookings.select { |booking| booking.status == "accepted" }
+
+    # bookings variables
+
     @current_bookings = @user.bookings.select { |booking| booking.status == "accepted" }
     @future_bookings = @user.bookings.select { |booking| booking.status == "pending" }
     @past_bookings = @user.bookings.select { |booking| booking.status == "rejected" }
