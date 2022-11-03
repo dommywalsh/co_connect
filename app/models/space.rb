bfba13971a2dc75e22a09cdb836463970df5f3c8
Dominic Walsh <djcwalsh94@gmail.com>
2022-11-03 19:49:06 +0200

    css is actually hard

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,14 +1,11 @@
 class Space < ApplicationRecord
   belongs_to :user
   has_many :bookings
-
-
   has_many_attached :images
 
   has_one_attached :image
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
 
   include PgSearch::Model
 
@@ -16,5 +13,5 @@ class Space < ApplicationRecord
               against: [ :name, :description ],
               using: {
                 tsearch: { any_word: true }
-}
+                      }
 end
